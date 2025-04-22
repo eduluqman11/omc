8ff22d269abf08bfffc2a83275b1c051b7957310
luqmanEdulab <luqman@edulab.in>
2025-04-22 19:44:06 +0530

    add quantity

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,10 +1,9 @@
 "use client"
+import OrderSummeryCard from "@/components/orderSummeryCard";
 import ShoppingCartCard from "@/components/shoppingCartProduct";
-import { Button } from "@/components/ui/button";
 import { useCart } from "@/context/cartContext";
 import { useQuery } from "@tanstack/react-query";
-import { MoveRight, X } from "lucide-react";
-import Link from "next/link";
+
 
 const fetchSProducts = async (ids: number[]) => {
   const promises = ids.map((id)=>{
@@ -25,34 +24,40 @@ export default function Cart(){
       const { cart , removeFromCart } = useCart();
       const getIdsOnly = cart.map((item)=>item.id)
         // Use query with id as a dependency
-        const { data, error, isLoading } = useQuery({
+        const { data, error, isLoading } = useQuery<any[]>({
           queryKey: ['cart-products', getIdsOnly],
           queryFn: () => fetchSProducts(getIdsOnly),
           enabled: getIdsOnly.length > 0,
         }); 
         
-        console.log('data:',data)
+        const merged = data?.map((product) => {
+          const cartItem = cart.find((item) => item.id === product.id);
+          return {
+            ...product,
+            quantity: cartItem?.quantity || 1, // fallback if not found
+          };
+        }) || [];
+
         const handleRemoveProduct = (productId: string) => {
           removeFromCart(Number(productId))
-          // filter it out from the products array or call API etc.
         };
 
     return (
         <section className="bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
         {
-         data !== undefined ? 
+         merged !== undefined ? 
          <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
          <h2 className="text-gray-900 title-font text-2xl font-medium font-bold">Shopping Cart</h2>
-     
          <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
            <div className="mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl">
-                  {data.map((product:any) => (
+                  {merged.map((product: any) => (
                     <ShoppingCartCard
                       key={product.id}
                       id={product.id}
                       image={product.images[0]}
                       price={product.price}
                       description={product.description}
+                      quantity={product.quantity || 0}
                       onRemove={handleRemoveProduct}
                     />
                   ))}
@@ -98,62 +103,8 @@ export default function Cart(){
              </div>
            </div>
      
-           <div className="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full">
-             <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6">
-               <p className="text-xl font-title text-gray-900 dark:text-white">Order summary</p>
-     
-               <div className="space-y-4">
-                 <div className="space-y-2">
-                   <dl className="flex items-center justify-between gap-4">
-                     <dt className="text-base font-title text-gray-500 dark:text-gray-400">Original price</dt>
-                     <dd className="text-base font-medium text-gray-900 dark:text-white">$7,592.00</dd>
-                   </dl>
-     
-                   <dl className="flex items-center justify-between gap-4">
-                     <dt className="text-base font-normal text-gray-500 dark:text-gray-400">Savings</dt>
-                     <dd className="text-base font-medium text-green-600">-$299.00</dd>
-                   </dl>
-     
-                   <dl className="flex items-center justify-between gap-4">
-                     <dt className="text-base font-normal text-gray-500 dark:text-gray-400">Store Pickup</dt>
-                     <dd className="text-base font-medium text-gray-900 dark:text-white">$99</dd>
-                   </dl>
-     
-                   <dl className="flex items-center justify-between gap-4">
-                     <dt className="text-base font-normal text-gray-500 dark:text-gray-400">Tax</dt>
-                     <dd className="text-base font-medium text-gray-900 dark:text-white">$799</dd>
-                   </dl>
-                 </div>
-     
-                 <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 dark:border-gray-700">
-                   <dt className="font-title text-xl text-gray-900 dark:text-white">Total</dt>
-                   <dd className="text-base font-title text-gray-900 dark:text-white">$8,191.00</dd>
-                 </dl>
-               </div>
-     
-             <div className="">
-             <a href="#" className="flex w-full items-center justify-center  px-2 py-2.5"><Button size={"lg"}>Proceed to Checkout</Button></a>
-             </div>
+     <OrderSummeryCard />
 
-               <div className="flex items-center justify-center gap-2">
-                 <span className="text-sm font-normal text-gray-500 dark:text-gray-400"> or </span>
-                 <Link href="/" title="" className="inline-flex items-center gap-2 text-sm font-medium text-primary-700 underline hover:no-underline dark:text-primary-500">
-                   Continue Shopping
-                   <MoveRight />
-                 </Link>
-               </div>
-             </div>
-     
-             <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6">
-               <form className="space-y-4">
-                 <div>
-                   <label htmlFor="voucher" className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"> Do you have a voucher or gift card? </label>
-                   <input type="text" id="voucher" className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500" placeholder="" required />
-                 </div>
-                 <button type="submit" className="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Apply Code</button>
-               </form>
-             </div>
-           </div>
          </div>
        </div>
        :<h2 className="text-center">No Cart Added!</h2>
