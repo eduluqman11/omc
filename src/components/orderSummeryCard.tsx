bb0f2ae93e7d4af872e2172d1f20e2f85a748523
luqmanEdulab <luqman@edulab.in>
2025-04-25 19:11:03 +0530

    add dialog and label from shadcn

--- src/components/orderSummeryCard.tsx
+++ src/components/orderSummeryCard.tsx
@@ -1,6 +1,17 @@
 import { MoveRight } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 interface IOrderSummery{
   OriginalPrice:number;
@@ -61,7 +72,39 @@ const OrderSummeryCard = ({OriginalPrice,saving,pickUpCharge,tax}:IOrderSummery)
           </div>
 
         <div className="">
-        <a href="#" className="flex w-full items-center justify-center  px-2 py-2.5"><Button size={"lg"}>Proceed to Checkout</Button></a>
+        <a className="flex w-full items-center justify-center  px-2 py-2.5">
+          <Dialog>
+      <DialogTrigger asChild>
+      <Button size={"lg"}>Proceed to Checkout</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+          </a>
+        
         </div>
 
           <div className="flex items-center justify-center gap-2">
